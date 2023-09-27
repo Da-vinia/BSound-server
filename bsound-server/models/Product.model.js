e962448e841dd3ffc9f6581a9b20c620b8eba205
Da-vinia <daviniatosco@gmail.com>
2023-09-27 23:55:37 +0200

    create models

---
+++ bsound-server/models/Product.model.js
@@ -0,0 +1,78 @@
+const { Schema, model } = require("mongoose");
+const mongoose = require('mongoose');
+
+const productSchema = new Schema(
+  {
+    productName: {
+      type: String,
+      required: [true, 'Product name is required.'],
+      trim: true,
+      min: 3,
+      max: 50,
+    },
+    category: {
+      type: String,
+      required: [true, 'A category is required.'],
+      enum: ['speakers', 'microphones', 'turntables', 'audio cables', 'instruments', 'lighting'],
+    },
+    description: {
+      type: String,
+      min: 10,
+      max: 100,
+    },
+    availableDays: {
+      type: [Date],
+      default: [],
+    },
+    mediaUrl:[ 
+        {
+            type: String,
+        },
+    ],
+    pricePerDay: {
+        type: Number,
+        required: [true, 'Price per day is required.'],
+    },
+    location: {
+        city: {
+            type: String,
+            default: 'Berlin',
+        },
+        district: {
+            type: String,
+            required: [true, 'A district is required.'],
+            enum: [
+                'Mitte',
+                'Friedrichshain-Kreuzberg',
+                'Pankow',
+                'Charlottenburg-Wilmersdorf',
+                'Spandau',
+                'Steglitz-Zehlendorf',
+                'Tempelhof-Schöneberg',
+                'Neukölln',
+                'Treptow-Köpenick',
+                'Marzahn-Hellersdorf',
+                'Lichtenberg',
+                'Reinickendorf',
+            ],
+        },
+    },
+    contactDetails: {
+        type: String,
+        required: [true, 'Email is required.'],
+        select: false,
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`    
+    timestamps: true
+  }
+);
+
+const Product = model("Product", productSchema);
+
+module.exports = Product;
