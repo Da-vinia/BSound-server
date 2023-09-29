0a37a1b39e24838d4dbb32b9772595a00f1f33b4
Da-vinia <daviniatosco@gmail.com>
2023-09-29 12:56:04 +0200

    fix edit profile

--- bsound-server/routes/user/profile.routes.js
+++ bsound-server/routes/user/profile.routes.js
@@ -7,22 +7,48 @@ const fileUploader = require("../../config/cloudinary.config");
 const { isAuthenticated } = require("./../../middleware/jwt.middleware");
 
 // PUT - edit user profile /profile/edit/:userId
-router.put('/:userId', isAuthenticated, fileUploader.single('avatar'), (req, res, next) => {
-    const { name } = req.body;
-    const { userId } = req.params; 
-    const avatar = req.file ? req.file.path : undefined;
+router.put('/', isAuthenticated, fileUploader.single('avatar'), (req, res, next) => {
+    let { name , email , avatar} = req.body;
+    const request = res.req;
+
+    const user = request.payload;
+
+    const userId = user._id;
+
+    console.log(userId);
+     
+    avatar = req.file ? req.file.path : undefined;
 
     User.findByIdAndUpdate( userId, { name, avatar , email  }, {new: true})
         .then((profileUpdated) => res.json(profileUpdated))
         .catch(error => res.json(error));
 });
 
-router.get("/", isAuthenticated, (req,res)=> {
 
+// this is the get route withe the list of products and list of bookings and listins that I'll create later and see on the user profile
+// router.get("/", isAuthenticated, (req,res)=> {
+
+//     const userId = req.payload._id
+//     User.findById(userId)
+//     .then((foundUser)=> {
+//        // console.log("Response from User",foundUser)
+//        Product.find().then((foundProducts) => res.json({user:foundUser, product:foundProduct, listing:foun}))
+//         res.status(200).json({user: foundUser })
+
+//         Booking.find({ booker: userId })
+//         .populate('productBooked') 
+//         .then((bookings) => res.status(200).json(bookings))
+//     })
+//     .catch((error)=> {
+//         console.log("Error response",error)
+//     })
+// })
+
+
+router.get("/", isAuthenticated, (req,res)=> {
     const userId = req.payload._id
     User.findById(userId)
     .then((foundUser)=> {
-       // console.log("Response from User",foundUser)
         res.status(200).json({user: foundUser })
     })
     .catch((error)=> {
@@ -32,27 +58,27 @@ router.get("/", isAuthenticated, (req,res)=> {
 
 // GET - to display only the products of the user
 // router.get('/profile/products/', isAuthenticated, (req, res) => {
-router.get("/user/:userId", (req, res) => {
-    const userId = req.params.userId;
+// router.get("/user/:userId", (req, res) => {
+//     const userId = req.params.userId;
       
-    Product.find({ owner: userId })
-        .then((products) => {
-            res.json(products);
-        })
-        .catch((error) => {
-            res.status(500).json({ error: "Error to get products of the user." });
-        });
-});
+//     Product.find({ owner: userId })
+//         .then((products) => {
+//             res.json(products);
+//         })
+//         .catch((error) => {
+//             res.status(500).json({ error: "Error to get products of the user." });
+//         });
+// });
 
 // GET - to display only the bookings of the user
-router.get('/user/:userId', isAuthenticated, (req, res, next) => {
-    const { userId } = req.params; 
+// router.get('/user/:userId', isAuthenticated, (req, res, next) => {
+//     const { userId } = req.params; 
 
-    Booking.find({ booker: userId })
-        .populate('productBooked') 
-        .then((bookings) => res.status(200).json(bookings))
-        .catch((error) => res.status(500).json({ message: "Error to get bookings of the user", error }));
-});
+//     Booking.find({ booker: userId })
+//         .populate('productBooked') 
+//         .then((bookings) => res.status(200).json(bookings))
+//         .catch((error) => res.status(500).json({ message: "Error to get bookings of the user", error }));
+// });
 // GET - to display only the listings of the user
 
 
