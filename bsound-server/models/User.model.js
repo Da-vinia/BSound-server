36c216983720b5a42e8554ecdd5f067109847b63
Da-vinia <daviniatosco@gmail.com>
2023-10-16 20:16:49 +0200

    change default avatar

--- bsound-server/models/User.model.js
+++ bsound-server/models/User.model.js
@@ -21,7 +21,8 @@ const userSchema = new Schema(
     },
     avatar: {
       type: String,
-      default: '/images/default-avatar.png'
+      // default: '/images/default-avatar.png'
+      default: '../assets/images/avatar-default.png'
     },
   },
   {
